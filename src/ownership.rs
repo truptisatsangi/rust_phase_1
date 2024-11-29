01b294f3258a2b6fc3e2c6bda10de8aaeda850aa
truptisatsangi <truptisatsangi18@gmail.com>
2024-11-29 10:12:51 +0530

    feat: double free error in ownership

--- src/ownership.rs
+++ src/ownership.rs
@@ -7,4 +7,18 @@ pub fn ownership() {
 
     println!("{s}"); // This will print `hello, world!`
 
+    // We can't copy the string to another one, if we do this then rust has to free up same memory twice which is a double
+    // free error, due to this what rust do when you copy string to another one it consider first one as invalid.
+    
+    let s1 = String::from("hello");
+    let s2 = s1;
+
+    // println!("{s1}, world!"); // error
+
+    // But if we still want to copy the string we can use clone which is expensive
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {s1}, s2 = {s2}");
+
 }
