e05eb4ef5148274867a2b49dd4d72c1cee6683d1
truptisatsangi <truptisatsangi18@gmail.com>
2024-12-02 10:09:46 +0530

    refactor: uncomment code

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,15 @@ mod fibonacci;
 mod ownership;
 
 fn main() {
-    // overflow_handlers:: overflow_handlers();
-    // println!("-------------------------------------------------------------");
-    // branches:: branches_demo();
-    // branches:: loop_demo();
-    // branches:: naming_the_loop()
-    //  let res = fibonacci:: generate_fibonacci(5);
-    //  println!("{res}");
-    // ownership:: ownership();
-    // ownership:: ownership_in_functions();
-    // ownership:: ownership_in_return();
+    overflow_handlers:: overflow_handlers();
+    println!("-------------------------------------------------------------");
+    branches:: branches_demo();
+    branches:: loop_demo();
+    branches:: naming_the_loop()
+     let res = fibonacci:: generate_fibonacci(5);
+     println!("{res}");
+    ownership:: ownership();
+    ownership:: ownership_in_functions();
+    ownership:: ownership_in_return();
     ownership:: return_multiple_values();
 }
